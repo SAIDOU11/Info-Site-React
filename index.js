4ce3cd9ef61794fff5de35c7db3010b6cfe0fa55
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-31 10:10:31 +0200

    why react

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-ReactDOM.render(<h1>I'm learning React</h1>, document.getElementById("root"));
+// JSX
+
+const navbar = (
+  <nav>
+    <h1>Website</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+
+ReactDOM.render(navbar, document.getElementById("root"));
